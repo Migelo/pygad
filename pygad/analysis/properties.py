correct temperature calculation
also restore the old tests for the X-ray luminosities

pygad/analysis/properties.py
@@ -103,9 +103,9 @@ Example:
     derive block temp... done.
     >>> s.gas['lx'].units
     Unit("erg s**-1")
-    >>> if abs(np.mean(s.gas['lx']) - '1.6e35 erg/s') > '0.15e35 erg/s':
+    >>> if abs(np.mean(s.gas['lx']) - '1.85e35 erg/s') > '0.15e35 erg/s':
     ...     print np.mean(s.gas['lx'])
-    >>> if abs(s.gas['lx'].sum() - '1.5e41 erg/s') > '0.15e41 erg/s':
+    >>> if abs(s.gas['lx'].sum() - '1.7e41 erg/s') > '0.15e41 erg/s':
     ...     print s.gas['lx'].sum()
 '''
 __all__ = ['mass_weighted_mean', 'center_of_mass', 'reduced_inertia_tensor',

pygad/snapshot/snapshot.py
@@ -27,7 +27,7 @@ Example:
     >>> s.loadable_blocks()
     ['vel', 'mass', 'ID', 'pos']
     >>> ' '.join(s.deriveable_blocks())
-    'Epot vx jzjc vy vz rcyl angmom momentum E dV vrad Ekin vcirc r jcirc y x z'
+    'Epot vx jzjc vy vz rcyl angmom momentum E dV vrad Ekin temp vcirc r jcirc y x z'
     >>> assert set(s.all_blocks()) == set(s.loadable_blocks() + s.deriveable_blocks())
     >>> mwgt_pos = np.tensordot(s['mass'], s['pos'], axes=1).view(UnitArr)
     load block mass... done.