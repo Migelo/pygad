d5864a9334215ac702dc860492ec0b939f5cfd10
Bernhard Roettgers <broett@mpa-garching.mpg.de>
2016-10-11 12:42:19 +0200

    return kernel integ. in `mock_absorption_spectrum`
    * instead of returning the tau contribution, return the (computationally
      more demanding) kernel integral for all the particles ("restricted to
      the chosen spectrum")
    * bug fix: usage of the region of interest limits in the C++ code
    * reorder the argument order in `mock_absorption_spectrum[_of]`

--- pygad/C/src/absorption_spectra.cpp
+++ pygad/C/src/absorption_spectra.cpp
@@ -20,7 +20,7 @@ void _absorption_spectrum(size_t N,
                           double *los_dens,
                           double *los_temp,
                           double *v_lims,
-                          double *contributions,
+                          double *kernel_int,
                           const char *kernel_,
                           double periodic) {
     double dv = (vel_extent[1] - vel_extent[0]) / Nbins;
@@ -37,7 +37,7 @@ void _absorption_spectrum(size_t N,
 
 #pragma omp parallel for default(shared) schedule(dynamic,10)
     for (size_t j=0; j<N; j++) {
-        contributions[j] = 0.0;  // for proper values when skipping
+        kernel_int[j] = 0.0;    // for proper values when skipping
         double Nj = n[j];
 
         if ( particles ) {
@@ -50,6 +50,7 @@ void _absorption_spectrum(size_t N,
                 continue;
             double Wj = kernel.proj_value(dj/hj, hj);
             Nj *= Wj;
+            kernel_int[j] = Wj;
         }
 
         // column density of the particles / cells along the line of sight
@@ -62,8 +63,10 @@ void _absorption_spectrum(size_t N,
         // thermal broadening tb_b(v) = 1/(b*sqrt(pi)) * exp( -(v/b)^2 )
         double b = b_0 * std::sqrt(Tj);
         constexpr double int_width = 8.0;   // go out to this times the rms
-        if ( vj+int_width*b < vel_extent[0] or vj-int_width*b > vel_extent[1] )
+        if ( vj+int_width*b < vel_extent[0] or vj-int_width*b > vel_extent[1] ) {
+            kernel_int[j] = 0.0;
             continue;   // out of bounds -- don't bin into bin #0 or #Nbins-1
+        }
         size_t vi_min = std::max<double>(0.0,     std::floor((vj-int_width*b - vel_extent[0]) / dv));
         size_t vi_max = std::min<double>(Nbins-1, std::ceil( (vj+int_width*b - vel_extent[0]) / dv));
 
@@ -76,15 +79,15 @@ void _absorption_spectrum(size_t N,
 #pragma omp atomic
             los_temp[vi_min] += Tj * Nj;
 
-            if( in_lims(vj,v_lims) )
-                contributions[j] = 1.0;
+            if( not in_lims(vj,v_lims) )
+                kernel_int[j] = 0.0;
         } else {
             double contrib_lim = 0.0;
-            double contrib_total = 0.0;
+            //double contrib_total = 0.0;
             // antiderivative of tb_b: int_0_v dv' tb_b(v') = 1/2 * erf(v/b)
             for ( size_t i=vi_min; i<=vi_max; i++ ) {
                 double Dtb;
-                double v = (i-vi) * dv;
+                double Dv = (i-vi) * dv;
                 if ( b < 10.*dv ) {
                     // standard deviation gets comparable with the bin size, do
                     // proper integrals of the line-profile over the bins
@@ -93,7 +96,7 @@ void _absorption_spectrum(size_t N,
                     Dtb = 0.5 * (std::erf(v1/b) - std::erf(v0/b));
                 } else {
                     // approximate the line as constant over the bin
-                    Dtb = std::exp(-std::pow(v/b,2.0)) / (b * std::sqrt(M_PI)) * dv;
+                    Dtb = std::exp(-std::pow(Dv/b,2.0)) / (b * std::sqrt(M_PI)) * dv;
                 }
                 double DtbNj = Dtb * Nj;
                 // TODO: addtional loop for the Lorentz profile
@@ -104,12 +107,12 @@ void _absorption_spectrum(size_t N,
 #pragma omp atomic
                 los_temp[i] += Tj * DtbNj;
 
-                contrib_total += Dtb;
-                if ( in_lims(v,v_lims) )
+                //contrib_total += Dtb;
+                if ( in_lims(vj+Dv,v_lims) )
                     contrib_lim += Dtb;
             }
 
-            contributions[j] = contrib_lim/contrib_total;
+            kernel_int[j] *= contrib_lim; // / contrib_total;
         }
     }
 
@@ -139,7 +142,7 @@ void absorption_spectrum(bool particles,
                          double *los_dens,
                          double *los_temp,
                          double *v_lims,
-                         double *contributions,
+                         double *kernel_int,
                          const char *kernel_,
                          double periodic) {
     if ( particles ) {
@@ -147,14 +150,14 @@ void absorption_spectrum(bool particles,
                                           los_pos, vel_extent, Nbins,
                                           b_0, Xsec,
                                           taus, los_dens, los_temp,
-                                          v_lims, contributions,
+                                          v_lims, kernel_int,
                                           kernel_, periodic);
     } else {
         return _absorption_spectrum<false>(N, pos, vel, hsml, n, temp,
                                            los_pos, vel_extent, Nbins,
                                            b_0, Xsec,
                                            taus, los_dens, los_temp,
-                                           v_lims, contributions,
+                                           v_lims, kernel_int,
                                            kernel_, periodic);
     }
 }
